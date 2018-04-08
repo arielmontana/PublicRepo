7051532301efc3a7b1ada4f2389888309daf55da
arielmontana <38056351+arielmontana@users.noreply.github.com>
2018-04-08 11:20:43 -0300

    Se agrega Route para cambiar nombre de publicaciòn de un metodo de controller

---
+++ WebApiRoute/Routing/Controllers/LeagueController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Routing.Controllers
+{
+    public class LeagueController : ApiController
+    {
+        [HttpGet]
+        [Route("Import-League/{leagueCode}")]
+        public string Team(string leagueCode)
+        {
+            return string.Format("El código de League que se recibió fue: '{0}'", leagueCode);
+        }
+    }
+}
